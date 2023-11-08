57b3256f640a43fd0cbff5e7973452e528d0bb82
stino-x <107426247+stino-x@users.noreply.github.com>
2023-11-08 11:52:24 +0100

    create 18 files and update 1 file

---
+++ db/migrate/20231108093052_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
+
+      # Add the author_id column to reference the users table
+      t.references :author, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
