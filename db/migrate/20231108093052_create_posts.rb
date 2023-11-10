929b7a55acde7a9709d535c2a67790c9a7321a6a
stino-x <107426247+stino-x@users.noreply.github.com>
2023-11-10 14:07:46 +0100

    This commit: * Updates the column name from `content` to `text` in the `posts` table. * Removes the migration for adding a `photo` column to the `users` table.

--- db/migrate/20231108093052_create_posts.rb
+++ db/migrate/20231108093052_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.text :content, null: false
+      t.text :text, null: false
       t.integer :comments_counter, default: 0
       t.integer :likes_counter, default: 0
 
