8bab8d9978196d273ff5f227046a2c3b0b71e0f0
stino-x <107426247+stino-x@users.noreply.github.com>
2023-11-10 20:36:03 +0100

    The diff shows that the following changes have been made to the code:
    1. In the `Comment` model, the method `update_post_comments_counter` has been changed to use `increment!` method to update the `comments_counter` column of the associated `Post` model.
    
    2. In the `Like` model, the `after_create` and `after_destroy` callbacks have been added to call the `update_likes_counter` method, which has been moved to the private section of the class. The method has also been changed to use `increment!` method to update the `likes_counter` column of the associated `Post` model.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,17 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_create :update_likes_counter
+  after_destroy :update_likes_counter
 
-  class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    after_create :update_likes_counter
-    after_destroy :update_likes_counter
+  private
 
-    private
-
-    def update_likes_counter
-      post.update(likes_counter: likes.count)
-    end
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
