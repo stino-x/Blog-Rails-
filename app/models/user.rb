958a903a4cdab67d2857007cb16c84d7cd95ae27
stino-x <107426247+stino-x@users.noreply.github.com>
2023-11-13 18:21:38 +0100

    This commit removes the validation for the bio field, allowing it to be nil.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   validates :name, presence: true, length: { maximum: 100 }
-  # validates :bio, allow_nil: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
