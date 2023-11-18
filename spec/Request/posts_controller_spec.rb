896b2eadf9dd64b93af4a0312c2ac349d40a906b
stino-x <107426247+stino-x@users.noreply.github.com>
2023-11-18 23:38:54 +0100

    Update views and remove unnecessary controller specs

--- spec/Request/posts_controller_spec.rb
+++ spec/Request/posts_controller_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
       user = User.create(name: 'John Doe')
 
-      get :index, params: { user_id: user.id }
+      get user_posts_path(user)
       expect(response).to be_successful
     end
 
     it 'renders the index template' do
       user = User.create(name: 'John Doe')
 
-      get :index, params: { user_id: user.id }
+      get user_posts_path(user)
       expect(response).to render_template(:index)
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      user = User.create(name: 'John Doe')
+
+      get user_posts_path(user)
+      expect(response.body.to_s).to include('Here is a list of all the posts from this user')
     end
   end
 
@@ -22,8 +29,7 @@ RSpec.describe PostsController, type: :controller do
       user = User.create(name: 'John Doe')
       post = Post.create(title: 'Sample Post', text: 'something', author_id: user.id)
 
-      # Use the route helper method to generate the path
-      get :show, params: { id: post.id, user_id: user.id }
+      get user_post_path(user, post)
       expect(response).to be_successful
     end
 
@@ -31,9 +37,16 @@ RSpec.describe PostsController, type: :controller do
       user = User.create(name: 'John Doe')
       post = Post.create(title: 'Sample Post', text: 'something', author_id: user.id)
 
-      # Use the route helper method to generate the path
-      get :show, params: { id: post.id, user_id: user.id }
+      get user_post_path(user, post)
       expect(response).to render_template(:show)
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      user = User.create(name: 'John Doe')
+      post = Post.create(title: 'Sample Post', text: 'something', author_id: user.id)
+
+      get user_post_path(user, post)
+      expect(response.body.to_s).to include('Here is the info for a particular post')
     end
   end
 end
--- spec/Request/user_controller_spec.rb
+++ spec/Request/user_controller_spec.rb
@@ -1,15 +1,15 @@
-# spec/controllers/users_controller_spec.rb
+# spec/requests/users_controller_spec.rb
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get :index
+      get users_path
       expect(response).to be_successful
     end
 
     it 'renders the index template' do
-      get :index
+      get users_path
       expect(response).to render_template(:index)
     end
 
@@ -17,8 +17,13 @@ RSpec.describe UsersController, type: :controller do
       user1 = User.create(name: 'John Doe')
       user2 = User.create(name: 'Jane Doe')
 
-      get :index
+      get users_path
       expect(assigns(:users)).to eq([user1, user2])
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      get users_path
+      expect(response.body.to_s).to include('Here is a list of all posts for a user')
     end
   end
 
@@ -26,7 +31,7 @@ RSpec.describe UsersController, type: :controller do
     context 'with valid parameters' do
       it 'creates a new user' do
         expect do
-          post :create, params: { user: { name: 'John Doe' } }
+          post users_path, params: { user: { name: 'John Doe' } }
         end.to change(User, :count).by(1)
       end
     end
@@ -35,20 +40,26 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #show' do
     it 'returns a successful response' do
       user = User.create(name: 'John Doe')
-      get :show, params: { id: user.id }
+      get user_path(user)
       expect(response).to be_successful
     end
 
     it 'renders the show template' do
       user = User.create(name: 'John Doe')
-      get :show, params: { id: user.id }
+      get user_path(user)
       expect(response).to render_template(:show)
     end
 
     it 'assigns the requested user to @user' do
       user = User.create(name: 'John Doe')
-      get :show, params: { id: user.id }
+      get user_path(user)
       expect(assigns(:user)).to eq(user)
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      user = User.create(name: 'John Doe')
+      get user_path(user)
+      expect(response.body.to_s).to include('Here is the info for a particular post')
     end
   end
 end
