c132eb462950a1c03b189d226cb3057ca33dbdf9
stino-x <107426247+stino-x@users.noreply.github.com>
2023-12-01 14:48:48 +0100

    Update user model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users do
-    resources :posts do
-      resources :likes, only: [:create, :destroy]
-      resources :comments
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
     end
   end
   # Defines the root path route ("/")
